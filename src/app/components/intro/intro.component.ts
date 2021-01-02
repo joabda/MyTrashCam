e96e949dce79529a1ad3b7d7010733132c24c8d0
joabda <joe-abdo3@hotmail.com>
2021-01-02 14:22:15 -0500

    Adding shortcuts to pause, next and previous photo in homescreen

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -1,28 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { HotkeysService } from 'src/app/services/hotkeys/hotkeys.service';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.component.html',
   styleUrls: ['./intro.component.scss']
 })
-export class IntroComponent {
+export class IntroComponent implements OnDestroy {
 
   static TIME_IN_SECONDS: number = 1.5;
   static NUMBER_OF_IMAGES: number = 8;
   private counter: number = 0;
+  private subscriptions: Subscription[] = [];
+  private timer: NodeJS.Timer;
+  private timerIsOn: boolean;
 
-  constructor() { 
-    setInterval(()=> { this.changeImage() }, IntroComponent.TIME_IN_SECONDS * 1000);
+  constructor(private shortcuts: HotkeysService) {
+    this.timer = this.setTimer();
+    this.timerIsOn = true;
   }
 
-  private changeImage(): void {
-    document.getElementById("home-image").style.backgroundImage = 
-      `url(../../../assets/img/homepage/${this.getNext()}.JPG)`
+  ngOnInit() {
+    this.subscriptions.push(this.shortcuts.addShortcut({ keys: "space", description: "Pause/Play" }).subscribe( () => this.pause()));
+
+    this.subscriptions.push(this.shortcuts.addShortcut({ keys: "arrowleft", description: "Next Picture" }).subscribe((_event) => {
+      this.timerIsOn = true;
+      this.pause();
+      this.changeImage(this.getPrevious());
+    }));
+
+    this.subscriptions.push(this.shortcuts.addShortcut({ keys: "arrowright", description: "Previous Picture" }).subscribe((_event) => {
+      this.timerIsOn = true;
+      this.pause();
+      this.changeImage(this.getNext());
+    }));
+  }
+
+  ngOnDestroy(): void {
+    for (let i: number = this.subscriptions.length - 1; i >= 0; --i) {
+      this.subscriptions[i].unsubscribe();
+      this.subscriptions.pop();
+    }
+  }
+  
+  private setTimer(): NodeJS.Timer{
+    return setInterval(() => { this.changeImage(this.getNext()) }, IntroComponent.TIME_IN_SECONDS * 1000);
+  }
+
+  private pause(): void {
+    if(this.timerIsOn) {
+      this.timerIsOn = false;
+      clearInterval(this.timer);
+    } else {
+      this.timerIsOn = true;
+      this.timer = this.setTimer();
+    }
+  }
+
+  private changeImage(index: number): void {
+    document.getElementById("home-image").style.backgroundImage =
+      `url(../../../assets/img/homepage/${index}.JPG)`
   }
 
   private getNext(): number {
-    if(++this.counter >= IntroComponent.NUMBER_OF_IMAGES) {
+    if (++this.counter >= IntroComponent.NUMBER_OF_IMAGES) {
       this.counter = 0;
+    }
+    return this.counter;
+  }
+
+  private getPrevious(): number {
+    if (--this.counter <= 0) {
+      this.counter = IntroComponent.NUMBER_OF_IMAGES - 1;
     }
     return this.counter;
   }
