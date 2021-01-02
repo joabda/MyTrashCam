8ac055b01d56dc050af3a5691bece42061a772cc
joabda <joe-abdo3@hotmail.com>
2021-01-01 20:12:54 -0500

    Slideshow welcome page

--- src/app/components/intro/intro.component.ts
+++ src/app/components/intro/intro.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./intro.component.scss']
 })
 export class IntroComponent {
-  constructor() { }
+
+  static TIME_IN_SECONDS: number = 1;
+  static NUMBER_OF_IMAGES: number = 5;
+  private counter: number = 0;
+
+  constructor() { 
+    setInterval(()=> { this.changeImage() }, IntroComponent.TIME_IN_SECONDS * 1000);
+  }
+
+  private changeImage(): void {
+    document.getElementById("home-image").style.backgroundImage = 
+      `url(../../../assets/img/homepage/${this.getNext()}.jpg)`
+  }
+
+  private getNext(): number {
+    if(++this.counter >= IntroComponent.NUMBER_OF_IMAGES) {
+      this.counter = 0;
+    }
+    return this.counter;
+  }
 }
